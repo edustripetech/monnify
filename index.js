f7b05f00ff5186b5149add858c75086ecfab3bcb
ngwobia, chukwudi mike <ngwobiachukwudi@gmail.com>
2020-08-22 17:24:27 +0100

    implement reserve account

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
-const axiosInstance = require('./utils/axios');
+const getAxiosInstance = require('./utils/axios');
+const config = require('./config');
+
+const user = {
+  id: 1,
+  name: 'Esho, Tolulope',
+  email: 'esho.tolu@edustripe.com'
+}
 
 // Reserving an account
+function reserveAccount() {
+  const accountData = {
+    accountReference: `${user.name}'s reserved-account`,
+    accountName: user.name,
+    currencyCode: "NGN",
+    contractCode: config.monnifyContractCode,
+    customerEmail: user.email,
+    customerName: user.name,
+  };
+
+  return new Promise(function (resolve, reject) {
+    getAxiosInstance()
+      .then(function (axiosInstance) {
+        axiosInstance.post('/bank-transfer/reserved-accounts', accountData)
+          .then(function ({ data }) {
+            return resolve(data);
+          })
+          .catch(function (err) {
+            return reject(err);
+          })
+      });
+  })
+}
+
+// demo the reserve account endpoint
+// reserveAccount()
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
+// The response from the above
+/*
+  {
+    requestSuccessful: true,
+    responseMessage: 'success',
+    responseCode: '0',
+    responseBody: {
+      contractCode: '2973248496',
+      accountReference: "Esho, Tolulope's reserved-account",
+      accountName: 'Esho, Tolulope',
+      currencyCode: 'NGN',
+      customerEmail: 'esho.tolu@edustripe.com',
+      customerName: 'Esho, Tolulope',
+      accountNumber: '4000047339',
+      bankName: 'Providus Bank',
+      bankCode: '101',
+      collectionChannel: 'RESERVED_ACCOUNT',
+      reservationReference: 'XJWVJ5V9WTLJWT3CLD74',
+      reservedAccountType: 'GENERAL',
+      status: 'ACTIVE',
+      createdOn: '2020-08-22 17:12:55.411',
+      incomeSplitConfig: [],
+      restrictPaymentSource: false
+    }
+  }
+*/
